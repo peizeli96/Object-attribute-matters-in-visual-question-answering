26d36a41a8366830fed2849319acf320293f40bb
peizeli96 <96230295+peizeli96@users.noreply.github.com>
2025-03-13 17:31:48 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -134,16 +134,14 @@ def _load_qa_dataset(dataroot, name):
 
 
 
-
-
 class VQAFeatureDataset(Dataset):
     def __init__(self, opt, name, dictionary):
         super(VQAFeatureDataset, self).__init__()
-        assert name in ['train', 'valid', 'test']
+        assert name in ['train', 'test']
 
 
-        ans2label_path = os.path.join('../data/', 'trainval_ans2label.pkl')
-        label2ans_path = os.path.join('../data/', 'trainval_label2ans.pkl')
+        ans2label_path = os.path.join('data/', 'train_ans2label.pkl')
+        label2ans_path = os.path.join('data/', 'train_label2ans.pkl')
         self.ans2label = cPickle.load(open(ans2label_path, 'rb'))
         self.label2ans = cPickle.load(open(label2ans_path, 'rb'))
         self.num_ans_candidates = len(self.ans2label)
@@ -152,26 +150,36 @@ class VQAFeatureDataset(Dataset):
         print('loading image features and bounding boxes')
         # Load image features and bounding boxes
         
-        with open('../data/_img_feature_%s.pickle' % name, 'rb') as f:
+
+        
+        with open('data/vqacp_v1_train_img_feat.pickle', 'rb') as f:
             self.pretrain_feature = pickle.load(f)
+        with open('data/vqacp_v1_test_img_feat.pickle', 'rb') as f:
+            self.pretrain_feature.update(pickle.load(f))      
+          
 
         print('loading image features and bounding boxes done!')
 
-        self.entries = _load_qa_dataset("../data", name)
+        self.entries = _load_qa_dataset("data", name)
 
-        self.kb_features = json.load(open('../data/%s_kb.json' % name)) 
-        self.kb_token = {}
-        
+        self.blip_features = json.load(open('data/vqacpv2_%s_blip.json' % name)) 
+        self.blip_token = {}
+        self.blip2_features = json.load(open('data/vqacp2_%s_blip2.json' % name)) 
+        self.blip2_token = {}
+        self.ofa_features = json.load(open('data/vqacp2_%s_ofa_qa.json' % name)) 
+        self.ofa_token = {}
+        self.konw = json.load(open('data/vqacp_v2_%s_caption_blip2.json'% name))
+        self.kb = {}        
 
-        self.konw = json.load(open('../data/kb_%s_krvqa_dep.json'% name))
-        self.kb = {}
-        
-        self.attribute = json.load(open("../data/%s_attribute.json" % name))
+        self.attribute = json.load(open("data/vqacp2_%s_attribute.json" % name))
         self.attribute_feature = {}
-
-        self.lxmert_tokenize(18)
+        
+        
+        self.lxmert_tokenize(12)
         self.blip_tokenize(1)
-        self.kb_tokenize(10)
+        self.ofa_tokenize(1)
+        self.blip2_tokenize(1)
+        self.kb_tokenize(12)
         self.attribute_tokenize(40)
 
 
@@ -179,17 +187,37 @@ class VQAFeatureDataset(Dataset):
 
 
 
-
-    def lxmert_tokenize(self, max_length=18):
+    def lxmert_tokenize(self, max_length=14):
         """Tokenizes the questions.
 
         This will add q_token in each entry of the dataset.
         -1 represent nil, and should be treated as padding_idx in embedding
         """
-
+        tokenizer = LxmertTokenizer.from_pretrained('../lxmert_cache')
         for entry in self.entries:
-            lower_question_text = entry['question'] 
-            tokens = self.dictionary.tokenize(lower_question_text, False)
+            question_text = entry['question'] 
+            lower_question_text = question_text.lower()
+            q_tokens_dict = tokenizer(lower_question_text)
+            q_tokens = q_tokens_dict['input_ids']
+
+            # length = len(q_tokens)
+
+            if len(q_tokens) > max_length:
+                q_tokens = q_tokens[:max_length]
+            else:
+                padding = [tokenizer('[PAD]')['input_ids'][1:-1][0]] * (max_length - len(q_tokens))
+                q_tokens = q_tokens + padding
+           
+            utils.assert_eq(len(q_tokens), max_length)
+            
+            entry['sent'] = q_tokens
+
+
+    def kb_tokenize(self, max_length=6):
+
+        for qid in self.konw:
+            lxmert_word = self.konw[qid]
+            tokens = self.dictionary.tokenize(lxmert_word, False)
 
             if len(tokens) < max_length:
                 padding = [self.dictionary.padding_idx] * (max_length - len(tokens))
@@ -198,22 +226,8 @@ class VQAFeatureDataset(Dataset):
             else:
                 tokens = tokens[:max_length] 
             
-            entry['sent'] = tokens
-
-
-    def kb_tokenize(self, max_length=6):
-
-        for qid in self.konw:
-            lxmert_word = self.konw[qid]
-            # import pdb; pdb.set_trace()
-            tokens = self.dictionary.tokenize(lxmert_word, False)
-
-            if len(tokens) < max_length:
-                padding = [self.dictionary.padding_idx] * (max_length - len(tokens))
-                tokens = tokens + padding
-            else:
-                tokens = tokens[:max_length] 
             self.kb[qid] = tokens
+
 
 
     def blip_tokenize(self, max_length=14):
@@ -225,16 +239,45 @@ class VQAFeatureDataset(Dataset):
             if len(tokens) < max_length:
                 padding = [self.dictionary.padding_idx] * (max_length - len(tokens))
                 tokens = tokens + padding
+
             else:
                 tokens = tokens[:max_length] 
-                
+        
+            
             self.blip_token[qid] = tokens
-   
             
-            
-            
-            
+    def blip2_tokenize(self, max_length=14):
 
+        for qid in self.blip2_features:
+            lxmert_word = self.blip2_features[qid]
+            tokens = self.dictionary.tokenize(lxmert_word, False)
+
+            if len(tokens) < max_length:
+                padding = [self.dictionary.padding_idx] * (max_length - len(tokens))
+                tokens = tokens + padding
+
+            else:
+                tokens = tokens[:max_length] 
+        
+            
+            self.blip2_token[qid] = tokens
+            
+    def ofa_tokenize(self, max_length=14):
+
+        for qid in self.ofa_features:
+            lxmert_word = self.ofa_features[qid]
+            tokens = self.dictionary.tokenize(lxmert_word, False)
+
+            if len(tokens) < max_length:
+                padding = [self.dictionary.padding_idx] * (max_length - len(tokens))
+                tokens = tokens + padding
+
+            else:
+                tokens = tokens[:max_length] 
+        
+            
+            self.ofa_token[qid] = tokens            
+            
 
     def attribute_tokenize(self, max_length=18):
 
@@ -259,6 +302,20 @@ class VQAFeatureDataset(Dataset):
         
         for entry in self.entries:
             answer = entry['answer']
+            
+            label_id = []
+            for lab_id in entry['answer_token']:
+                if lab_id in self.label2ans:
+                    label_id.append(self.ans2label[lab_id])
+                
+                else:
+                    lab_id = ''
+                    label_id.append(self.ans2label[lab_id])
+
+
+            entry["label_id"] = label_id            
+            
+    
 
             ans_token = torch.from_numpy(np.array(answer['labels']))
             entry['answer_token'] = ans_token            
@@ -291,9 +348,12 @@ class VQAFeatureDataset(Dataset):
         features = torch.from_numpy(np.array(self.pretrain_feature[img_id]['feats'], dtype=float)).float()
         spatials = torch.from_numpy(np.array(self.pretrain_feature[img_id]['sp_feats'], dtype=float)).float()
 
-        blip_token = torch.from_numpy(np.array(self.blip_token[question_id]))
-        kb_token = torch.from_numpy(np.array(self.kb[question_id]))
-        attribute_token = torch.from_numpy(np.array(self.attribute_feature[img_id]))
+        blip_token = torch.from_numpy(np.array(self.blip_token[str(question_id)]))
+        blip2_token = torch.from_numpy(np.array(self.blip2_token[str(question_id)]))
+        ofa_token = torch.from_numpy(np.array(self.ofa_token[str(question_id)]))
+        kb_token = torch.from_numpy(np.array(self.kb[str(img_id)]))
+        attribute_token = torch.from_numpy(np.array(self.attribute_feature[str(img_id)]))
+
                                     
 
         if None != answer:
@@ -304,12 +364,14 @@ class VQAFeatureDataset(Dataset):
                 target.scatter_(0, labels, scores)
 
 
-            return features, spatials, question, target, question_id, kb_token, attribute_token, blip_token
+            return features, spatials, question, target, question_id, kb_token, attribute_token, blip_token, blip2_token, ofa_token
         else:
 
-            return features, spatials, question, question_id, kb_token, attribute_token, blip_token
+            return features, spatials, question, question_id, kb_token, attribute_token, blip_token, blip2_token, ofa_token
         
     def __len__(self):
         return len(self.entries)
+
+
 
 
