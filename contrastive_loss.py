161fca73d13ebd17f8472fcc13579082c7bae55a
peizeli96 <96230295+peizeli96@users.noreply.github.com>
2025-04-28 08:44:00 -0700

    Update contrastive_loss.py

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -1,11 +1,6 @@
 import torch
 from torch import nn
-device = torch.device("cuda:0")
 
-def distanceL2(h, t):
-    s = h - t
-    sum = torch.square(s).sum(-1)
-    return sum
 
 def dot_sim(im, s):
     """Cosine similarity between all the image and sentence pairs
@@ -13,22 +8,14 @@ def dot_sim(im, s):
     return im.mm(s.t())
 
 
-def l2_sim(im, s):
-    b_im = im.shape[0]
-    b_s = s.shape[0]
-    return distanceL2(im.unsqueeze(0).repeat(b_s,1,1),s.unsqueeze(1).repeat(1,b_im,1)).transpose(0,1)
-
-
 class ContrastiveLoss(nn.Module):
     """
     Compute contrastive loss
     """
 
-    def __init__(self, margin=1.0, measure=False, max_violation=False):
+    def __init__(self, measure=False, margin=1.0, max_violation=False):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
-        if measure == 'l2':
-            self.sim = l2_sim
         if measure == 'dot':
             self.sim = dot_sim
 
